a5a9ab0bd6fa54841c1c4b40b02ec9f98040dd01
George Anya <georgeanya4real@gmail.com>
2023-01-31 10:41:41 +0100

    commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,14 +19,13 @@ const SustainButton = styled(Button)({
 const Hero = () => {
   return (
     <div className="px-5 lg:px-32 lg:pt-22 pb-6 lg:pb-40 flex flex-col lg:flex-row lg:justify-between">
-      <div className="md:max-w-md lg:max-w-xl pt-15 pb-17 lg:pb-0 lg:pt-18">
-        <p className=" leading-tight lg:leading-tight  text-3xl lg:text-start lg:text-5xl font-bold text-[#002A47] ">
-          Say goodbye to chronic metabolic diseases and lifelong medication
+      <div className="md:max-w-md lg:max-w-lg pt-15 pb-17 lg:pb-0 lg:pt-18">
+        <p className=" leading-tight lg:leading-tight lg:pr-14  text-3xl lg:text-start lg:text-5xl font-bold text-[#002A47] ">
+          Say goodbye to <span className="text-[#4F9EEA]">type 2 diabetes</span> and lifelong medication
         </p>
-        <p className="mt-5 md:mt-6 text-base  lg:text-start lg:text-xl text-[#476D85]">
-          We help people reverse chronic metabolic diseases and reduce their
-          dependence on medications by identifying the root causes of disease
-          and treating them
+        <p className="mt-5 md:mt-6 text-base font-normal lg:pr-8 lg:text-start lg:text-xl text-[#476D85]">
+          Custodia helps people reverse chronic metabolic diseases like
+          diabetes, hypertension, and prediabetes using simple lifestyle changes
         </p>
         <div className="mt-8 lg:mt-14 ">
           <SustainButton className="text-xs lg:text-base font-medium">
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,130 +11,152 @@ const SustainButton = styled(Button)({
   margin: "0px 0px",
   borderRadius: "32px",
   textTransform: "none",
-  
 });
 
 const Navbar = () => {
   const [isToggled, setIsToggled] = useState(true);
 
   return (
-    <div className="px-5 lg:px-32 pt-5 lg:pt-5">
-      <nav>
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <img
-            src={sustain.src}
-            className="mr-3  self-center"
-            alt="Sustain Logo"
-          />
+    <div>
+      <div className="bg-[#324967] ">
+        <p className="text-center text-lg text-white py-4">
+          ✨ Take this 30-second assessment to know if you are at risk of type 2
+          diabetes
+        </p>
+      </div>
+      <div className="px-5 lg:px-32 pt-5 lg:pt-5">
+        <nav>
+          <div className="container flex flex-wrap justify-between items-center mx-auto">
+            <img
+              src={sustain.src}
+              className="mr-3  self-center"
+              alt="Sustain Logo"
+            />
 
-          <button
-            data-collapse-toggle="navbar-solid-bg"
-            type="button"
-            className="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden"
-            aria-controls="navbar-solid-bg"
-            onClick={() => {
-              setIsToggled(!isToggled);
-            }}
-          >
-            <span className="sr-only">Open main menu</span>
-            {isToggled ? (
-              <svg
-                className="w-6 h-6"
-                aria-hidden="false"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            )}
-          </button>
-
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-solid-bg"
-          >
-            <ul className="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-              <li className="mt-2.5 mx-8">
-                <a
-                  href="#"
-                  className="text-sm pt-5 mt-5 text-[#002A47] rounded  hover:text-blue-700 "
-                  // aria-current="page"
+            <button
+              data-collapse-toggle="navbar-solid-bg"
+              type="button"
+              className="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden"
+              aria-controls="navbar-solid-bg"
+              onClick={() => {
+                setIsToggled(!isToggled);
+              }}
+            >
+              <span className="sr-only">Open main menu</span>
+              {isToggled ? (
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="false"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-solid-bg"
+            >
+              <ul className="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+                <li className="mt-2.5 mx-5 ">
+                  <a
+                    href="#"
+                    className="text-sm pt-5 mt-5 text-[#002A47] rounded  hover:text-blue-700 "
+                    // aria-current="page"
+                  >
+                    For health plans
+                  </a>
+                </li>
+                <li className="mt-2.5 mx-5 ">
+                  <a
+                    href="#"
+                    className="text-sm pt-5 mt-5 text-[#002A47] rounded  hover:text-blue-700 "
+                  >
+                    For doctors
+                  </a>
+                </li>
+                
+                <li className="mt-2.5 mx-5 ">
+                  <a
+                    href="#"
+                    className="text-sm pt-5 mt-5 text-[#002A47] rounded  hover:text-blue-700 "
+                  >
+                    Pricing
+                  </a>
+                </li>
+                <li className="mt-2.5 mx-5 ">
+                  <a
+                    href="#"
+                    className="text-sm pt-5 mt-5 text-[#002A47] rounded hover:text-blue-700 md:p-0 "
+                  >
+                    Blog
+                  </a>
+                </li>
+                <li className="mt-2.5 mx-5 ">
+                  <a
+                    href="#"
+                    className="text-sm pt-5 mt-5 text-[#002A47] rounded  hover:text-blue-700 "
+                  >
+                    Research
+                  </a>
+                </li>
+                <li className="ml-5">
+                  <SustainButton className="text-sm">Get started</SustainButton>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div>
+          {isToggled ? (
+            <div className="hidden"></div>
+          ) : (
+            <ul className="pt-5">
+              <li>
+                <a href="#" className="block text-sm py-4">
                   Benefits
                 </a>
               </li>
-              <li className="mt-2.5 mx-8">
-                <a
-                  href="#"
-                  className="text-sm pt-5 mt-5 text-[#002A47] rounded  hover:text-blue-700 "
-                >
+              <li>
+                <a href="#services" className="block text-sm py-4">
                   Pricing
                 </a>
               </li>
-              <li className="mt-2.5 mx-8">
-                <a
-                  href="#"
-                  className="text-sm pt-5 mt-5 text-[#002A47] rounded hover:text-blue-700 md:p-0 "
-                >
+              <li>
+                <a href="#about" className="block text-sm py-4">
                   Blog
                 </a>
               </li>
-              <li className="ml-8">
-              <SustainButton className="text-sm">
-                  Get started
-                </SustainButton>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div>
-        {isToggled ? (
-          <div className="hidden"></div>
-        ) : (
-          <ul className="pt-5">
-            <li>
-              <a href="#" className="block text-sm py-4">
-                Benefits
-              </a>
-            </li>
-            <li>
-              <a href="#services" className="block text-sm py-4">
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a href="#about" className="block text-sm py-4">
-                Blog
-              </a>
-            </li>
-            {/* <li>
+              {/* <li>
               <SustainButton className="text-sm mt-5">
                 Get started
               </SustainButton>
             </li> */}
-          </ul>
-        )}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
