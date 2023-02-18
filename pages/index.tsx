edcea6eca626d5cf2758b63f7a4ce5f6b14eadb4
George Anya <georgeanya4real@gmail.com>
2023-02-18 14:36:43 +0100

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import ErrorPage from "./404";
-import Download from "../components/download";
+import Download from "./download";
 import { useState } from "react";
 
 import Hero from "../components/hero";
