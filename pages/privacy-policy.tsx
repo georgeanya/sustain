c667e2d133c2cecaa45390547356c6b07636ef0b
George Anya <georgeanya4real@gmail.com>
2023-03-24 21:22:41 +0100

    commit

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -9,21 +9,21 @@ const Policy = () => {
         <h2 className="text-4lg md:text-center lg:text-start lg:text-5xl font-bold text-[#002A47]">
           Privacy policy
         </h2>
-        <p className="mt-5 md:mt-6 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-6 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           Your privacy is important to us. It is Sustain&apos;s policy to
           respect your privacy regarding any information we may collect from you
           across our website, https://getsustain.app, and other sites we own and
           operate.
         </p>
 
-        <p className="mt-5 md:mt-7 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-7 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           We only ask for personal information when we truly need it to provide
           a service to you. We collect it by fair and lawful means, with your
           knowledge and consent. We also let you know why we&apos;re collecting
           it and how it will be used.
         </p>
 
-        <p className="mt-5 md:mt-7 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-7 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           We only retain collected information for as long as necessary to
           provide you with your requested service. What data we store,
           we&apos;ll protect within commercially acceptable means to prevent
@@ -31,32 +31,32 @@ const Policy = () => {
           use or modification.
         </p>
 
-        <p className="mt-5 md:mt-7 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-7 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           We don&apos;t share any personally identifying information publicly or
           with third-parties, except when required to by law.
         </p>
 
-        <p className="mt-5 md:mt-7 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-7 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           Our website may link to external sites that are not operated by us.
           Please be aware that we have no control over the content and practices
           of these sites, and cannot accept responsibility or liability for
           their respective privacy policies.
         </p>
 
-        <p className="mt-5 md:mt-7 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-7 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           You are free to refuse our request for your personal information, with
           the understanding that we may be unable to provide you with some of
           your desired services.
         </p>
 
-        <p className="mt-5 md:mt-7 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-7 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           Your continued use of our website will be regarded as acceptance of
           our practices around privacy and personal information. If you have any
           questions about how we handle user data and personal information, feel
           free to contact us.
         </p>
 
-        <p className="mt-5 md:mt-5 text-xlg md:text-center lg:text-start lg:text-xlg text-[#476D85]">
+        <p className="mt-5 md:mt-5 text-base md:text-center lg:text-start lg:text-xlg text-[#476D85]">
           This policy is effective as of 28 January 2021.
         </p>
       </div>
