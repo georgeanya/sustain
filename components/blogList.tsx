a9bd36f21a95fa198dd1f44ed720526d2f6d9955
George Anya <georgeanya4real@gmail.com>
2023-04-11 13:26:53 +0100

    commit

--- components/blogList.tsx
+++ components/blogList.tsx
@@ -1,11 +1,144 @@
-import React from 'react'
+import React, { useState } from "react";
+import BlogCard from "./blogCard";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+const SustainButton = styled(Button)({
+  color: "#4F9EEA !important",
+  fontFamily: "Circular Std",
+  background: "#f8f8f8",
+  padding: "20px 30px",
+  margin: "0px 0px",
+  borderRadius: "32px",
+  border: "solid 1px #4F9EEA",
+  textTransform: "none",
+  lineHeight: "20px",
+  ["@media (max-width:780px)"]: {
+    padding: "16px 30px",
+  },
+});
 
 const BlogList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [toggleState, setToggleState] = useState(1);
 
-export default BlogList
+  const toggleTab = (index: number) => {
+    setToggleState(index);
+  };
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>): any => {
+    setToggleState(Number(event.target.value));
+  };
+  return (
+    <div className="px-5 md:px-32 mb-24">
+      <div className="hidden md:block">
+        <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+          <li className="mr-2">
+            <p
+              className={
+                toggleState === 1
+                  ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
+                  : "inline-block px-6 py-3 text-[#476D85]"
+              }
+              onClick={() => toggleTab(1)}
+            >
+              All
+            </p>
+          </li>
+          <li className="mr-2">
+            <p
+              className={
+                toggleState === 2
+                  ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
+                  : "inline-block px-6 py-3 text-[#476D85]"
+              }
+              onClick={() => toggleTab(2)}
+            >
+              Patient stories
+            </p>
+          </li>
+          <li className="mr-2">
+            <p
+              className={
+                toggleState === 3
+                  ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
+                  : "inline-block px-6 py-3 text-[#476D85]"
+              }
+              onClick={() => toggleTab(3)}
+            >
+              Engineering
+            </p>
+          </li>
+          <li className="mr-2">
+            <p
+              className={
+                toggleState === 4
+                  ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
+                  : "inline-block px-6 py-3 text-[#476D85]"
+              }
+              onClick={() => toggleTab(4)}
+            >
+              Company
+            </p>
+          </li>
+          <li>
+            <p
+              className={
+                toggleState === 5
+                  ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
+                  : "inline-block px-6 py-3 text-[#476D85]"
+              }
+              onClick={() => toggleTab(5)}
+            >
+              Research
+            </p>
+          </li>
+          <li>
+            <p
+              className={
+                toggleState === 6
+                  ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
+                  : "inline-block px-6 py-3 text-[#476D85]"
+              }
+              onClick={() => toggleTab(6)}
+            >
+              Nutrition
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="block md:hidden">
+        <select
+          name="category"
+          onChange={handleSelect}
+          className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 bg-white h-11 md:h-12"
+        >
+          <option hidden selected>
+            Browse by category
+          </option>
+          <option value="1">All</option>
+          <option value="2">Patient stories</option>
+          <option value="3">Engineering</option>
+          <option value="4">Company</option>
+          <option value="5">Research</option>
+          <option value="6">Nutrition</option>
+        </select>
+      </div>
+      <div className="mt-10 grid md:grid-cols-3 md:gap-y-26 gap-15 md:mb-18 mb-15">
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+          </div>
+          <SustainButton>Show more posts</SustainButton>
+    </div>
+  );
+};
+
+export default BlogList;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,8 +4,10 @@ import Navbar1 from "../components/navbar1";
 import image from "../public/assets/image.svg";
 import a1ctest from "../public/assets/aictest.svg";
 import virus from "../public/assets/virus.svg";
+import BlogList from "../components/blogList";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
+
 
 const SustainButton = styled(Button)({
   background: "#4F9EEA !important",
@@ -14,7 +16,7 @@ const SustainButton = styled(Button)({
   padding: "20px 30px",
   margin: "0px 0px",
   borderRadius: "32px",
-    textTransform: "none",
+  textTransform: "none",
   lineHeight: "20px",
   ["@media (max-width:780px)"]: {
     padding: "16px 30px",
@@ -25,14 +27,14 @@ const Blog = () => {
   return (
     <div>
       <Navbar1 />
-      <div className="px-5 md:px-32 mb-24">
+      <div className="px-5 md:px-32 md:mb-24 mb-15">
         <p className=" text-sm md:text-llg font-normal mt-10 md:mt-14 text-[#002A47]">
           <span className=" font-bold">Blog</span> | Updates from the Custodia
           Health team
         </p>
         <div className="md:flex justify-between mt-9 md:mt-10">
           <img src={virus.src} alt="" />
-          <div className=" md:ml-17 mt-7 self-center">
+          <div className=" md:ml-17 mt-7 md:mt-0 self-center">
             <p className=" text-sm text-[#476D85]">Research</p>
             <p className="text-[#002A47] font-bold text-llg md:text-4lg mt-2.5 md:mt-3">
               What’s an A1C Test and Why Might You Need one?
@@ -44,17 +46,22 @@ const Blog = () => {
               prediabetes or type 2 diabetes.
             </p>
             <div className="flex mt-7 md:mt-10">
-              <img src={image.src} alt="" className="w-12"/>
+              <img src={image.src} alt="" className="w-12" />
               <div className="ml-4 self-center">
-                <p className="text-[#002A47] text-sm md:text-base">Dr. Yetunde Wonda</p>
+                <p className="text-[#002A47] text-sm md:text-base font-medium">
+                  Dr. Yetunde Wonda
+                </p>
                 <p className="text-[#476D85] text-xs">Clinical Ops</p>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <BlogList />
       <div className="px-5 md:px-32 md:pt-28 pt-20 md:pb-28 pb-20 bg-[#EFF2FA]">
-        <p className="md:text-4xl text-2xl text-[#002A47] font-bold max-w-[476px]">Stay updated by joining our newsletter</p>
+        <p className="md:text-4xl text-2xl text-[#002A47] font-bold max-w-[476px]">
+          Stay updated by joining our newsletter
+        </p>
         <p className=" text-bases md:text-xl text-[#002A47] max-w-[574px] mt-5 md:mt-6 mb-10 md:mb-12">
           Subscribe to recieve updates about our blog posts and announcements
           directly in your mailbox
