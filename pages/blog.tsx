a373e689d931a2d2853625a60718664ea2dd81e4
George Anya <georgeanya4real@gmail.com>
2023-05-03 23:19:53 +0100

    commit

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import Footer from "../components/footer";
 import Navbar1 from "../components/navbar1";
 import image from "../public/assets/image.svg";
@@ -31,26 +31,49 @@ interface Blog {
     title: string;
     description: string;
     content: string;
-    slug: any;
+    slug: string;
+    category: {
+      data: {
+        attributes: {
+          name: string;
+        };
+      };
+    };
+    author: {
+      data: {
+        attributes: {
+          name: string;
+        };
+      };
+    };
   };
 }
 
-// interface Props {
-//   blogs: Blog[];
-// }
+type BlogCategory = "All" | "Patient stories" | "Engineering" | "Company" | "Research" | "Nutrition"
 
 const BlogHome = ({ blogs }: any) => {
   console.log(blogs);
-  const [toggleState, setToggleState] = useState(1);
+  const [toggleState, setToggleState] = useState<BlogCategory>("All");
 
-  const toggleTab = (index: number) => {
+  const toggleTab = (index: BlogCategory) => {
     setToggleState(index);
   };
   const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>): any => {
-    setToggleState(Number(event.target.value));
+    setToggleState(event.target.value as BlogCategory);
   };
 
   const blog = blogs.data?.slice(0, 1)[0] || [];
+
+  const blogsToDisplay = useMemo(() => {
+    console.log(blogs.data);
+    
+    if (toggleState === "All") {
+      return blogs.data.slice(1)
+    }
+    return blogs.data?.slice(1).filter((blog: any) => {
+      return blog.attributes.category.data.attributes.name === toggleState
+    })
+  }, [blogs, toggleState]);
 
   return (
     <div>
@@ -65,7 +88,9 @@ const BlogHome = ({ blogs }: any) => {
             <img src={virus.src} alt="" className="cursor-pointer" />
           </Link>
           <div className=" md:ml-17 mt-7 md:mt-0 self-center">
-            <p className=" text-sm text-[#476D85]">Research</p>
+            <p className=" text-sm text-[#476D85]">
+              {blog.attributes.category.data.attributes.name}
+            </p>
             <Link href={`/blog/${blog.attributes.slug}`}>
               <p className="cursor-pointer text-[#002A47] font-bold text-llg md:text-4lg mt-2.5 md:mt-3">
                 {blog.attributes.title}
@@ -78,7 +103,7 @@ const BlogHome = ({ blogs }: any) => {
               <img src={image.src} alt="" className="w-12" />
               <div className="ml-4 self-center">
                 <p className="text-[#002A47] text-sm md:text-base font-medium">
-                  Dr. Yetunde Wonda
+                  {blog.attributes.author.data.attributes.name}
                 </p>
                 <p className="text-[#476D85] text-xs">Clinical Ops</p>
               </div>
@@ -93,11 +118,11 @@ const BlogHome = ({ blogs }: any) => {
               <li className="mr-2 cursor-pointer">
                 <p
                   className={
-                    toggleState === 1
+                    toggleState === "All"
                       ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
                       : "inline-block px-6 py-3 text-[#476D85]"
                   }
-                  onClick={() => toggleTab(1)}
+                  onClick={() => toggleTab("All")}
                 >
                   All
                 </p>
@@ -105,11 +130,11 @@ const BlogHome = ({ blogs }: any) => {
               <li className="mr-2 cursor-pointer">
                 <p
                   className={
-                    toggleState === 2
+                    toggleState === "Patient stories"
                       ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
                       : "inline-block px-6 py-3 text-[#476D85]"
                   }
-                  onClick={() => toggleTab(2)}
+                  onClick={() => toggleTab("Patient stories")}
                 >
                   Patient stories
                 </p>
@@ -117,11 +142,11 @@ const BlogHome = ({ blogs }: any) => {
               <li className="mr-2 cursor-pointer">
                 <p
                   className={
-                    toggleState === 3
+                    toggleState === "Engineering"
                       ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
                       : "inline-block px-6 py-3 text-[#476D85]"
                   }
-                  onClick={() => toggleTab(3)}
+                  onClick={() => toggleTab("Engineering")}
                 >
                   Engineering
                 </p>
@@ -129,11 +154,11 @@ const BlogHome = ({ blogs }: any) => {
               <li className="mr-2 cursor-pointer">
                 <p
                   className={
-                    toggleState === 4
+                    toggleState === "Company"
                       ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
                       : "inline-block px-6 py-3 text-[#476D85]"
                   }
-                  onClick={() => toggleTab(4)}
+                  onClick={() => toggleTab("Company")}
                 >
                   Company
                 </p>
@@ -141,11 +166,11 @@ const BlogHome = ({ blogs }: any) => {
               <li className="mr-2 cursor-pointer">
                 <p
                   className={
-                    toggleState === 5
+                    toggleState === "Research"
                       ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
                       : "inline-block px-6 py-3 text-[#476D85]"
                   }
-                  onClick={() => toggleTab(5)}
+                  onClick={() => toggleTab("Research")}
                 >
                   Research
                 </p>
@@ -153,11 +178,11 @@ const BlogHome = ({ blogs }: any) => {
               <li className="cursor-pointer">
                 <p
                   className={
-                    toggleState === 6
+                    toggleState === "Nutrition"
                       ? "inline-block px-5 py-3 text-white bg-[#4F9EEA] rounded-3xl active"
                       : "inline-block px-6 py-3 text-[#476D85]"
                   }
-                  onClick={() => toggleTab(6)}
+                  onClick={() => toggleTab("Nutrition")}
                 >
                   Nutrition
                 </p>
@@ -173,24 +198,29 @@ const BlogHome = ({ blogs }: any) => {
               <option hidden selected>
                 Browse by category
               </option>
-              <option value="1">All</option>
-              <option value="2">Patient stories</option>
-              <option value="3">Engineering</option>
-              <option value="4">Company</option>
-              <option value="5">Research</option>
-              <option value="6">Nutrition</option>
+              <option value="All">All</option>
+              <option value="Patient stories">Patient stories</option>
+              <option value="Engineering">Engineering</option>
+              <option value="Company">Company</option>
+              <option value="Research">Research</option>
+              <option value="Nutrition">Nutrition</option>
             </select>
           </div>
           <div className="mt-10 grid md:grid-cols-3 md:grid-rows-1 md:gap-y-26 gap-15 md:mb-18 mb-15">
-            {blogs.data?.slice(1).map((blogpost: any) => {
+            {blogsToDisplay.map((blogpost: any) => {
+              
               const blog = blogpost;
               const { id, attributes } = blog;
+              console.log(attributes.category);
+              
               return (
                 <Link href={`/blog/${attributes.slug}`} key={id}>
                   <div className="max-w-[357px]">
                     <img src={a1ctest.src} alt="" className="cursor-pointer" />
                     <div className="mt-7">
-                      <p className=" text-sm text-[#476D85]">Research</p>
+                      <p className=" text-sm text-[#476D85]">
+                        {attributes.category.data.attributes.name}
+                      </p>
                       <p className="text-[#002A47] font-bold text-llg md:text-1xl md:leading-8 mt-2.5 md:mt-3 cursor-pointer">
                         {attributes.title}
                       </p>
@@ -202,7 +232,7 @@ const BlogHome = ({ blogs }: any) => {
                           <img src={image.src} alt="" className="w-12" />
                           <div className="ml-4 self-center">
                             <p className="text-[#002A47] text-sm md:text-base md:leading-5 font-medium">
-                              Dr. Yetunde Wonda
+                              {attributes.author.data.attributes.name}
                             </p>
                             <p className="text-[#476D85] text-xs">
                               Clinical Ops
@@ -243,7 +273,7 @@ const BlogHome = ({ blogs }: any) => {
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await axios.get(
-    "https://custodia-health-blog.herokuapp.com/api/articles"
+    "https://custodia-health-blog.herokuapp.com/api/articles?populate[0]=category&populate[1]=author"
   );
   return {
     props: {
